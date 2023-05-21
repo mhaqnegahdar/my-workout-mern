996255669d8c3f537462975551125becc151a75c
Mohamad Haqnegahdar <104886566+maxjn@users.noreply.github.com>
2023-05-21 16:34:36 +0330

    Login & Signup : (#10)
    - added reusable AuthForm component
    - added Login & Signup pages
    - added Login & Signup links to the navbar
    - added style for AuthForm & the Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,15 @@ import {
 } from "react-router-dom";
 import Home from "./pages/Home";
 import { RootLayout } from "./layouts/RootLayout";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </Route>
   )
 );

5b076e00f9f49f808fb49224f53148dd74bd24f4
Mohamad Haqnegahdar <104886566+maxjn@users.noreply.github.com>
2023-05-22 20:19:58 +0330

    useLogout: (#12)
    - added logout hook
    - added logout button
    - added logout functionality
    - updated README.md

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,13 +1,18 @@
 import { NavLink } from "react-router-dom";
+import { useLogout } from "./../hooks/useLogout";
 
 const NavBar = () => {
+  const { logout } = useLogout();
+
   return (
     <header>
       <div className="container">
         <NavLink to="/">
           <h1>Workout Buddy</h1>
         </NavLink>
+
         <nav>
+          <button onClick={() => logout()}>Logout</button>
           <NavLink to="/login">Login</NavLink>
           <NavLink to="/signup">Signup</NavLink>
         </nav>

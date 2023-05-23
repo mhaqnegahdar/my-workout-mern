8ee3a03feaeed9e1c81e8e7c4c4de419ba6613ca
Mohamad Haqnegahdar <104886566+maxjn@users.noreply.github.com>
2023-05-23 09:23:23 +0330

    init Auth Status: (#14)
    - added conditional rendering to Logout & Signup buttons
    - added margin left to Lougout button
    - added getting init Auth Status to AuthContext

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,17 +1,10 @@
 import { NavLink } from "react-router-dom";
 import { useLogout } from "./../hooks/useLogout";
 import { useAuthContext } from "../hooks/useAuthContext";
-import { useEffect } from "react";
-import { getLocalStorage } from "../utils/localstorage";
 
 const NavBar = () => {
   const { logout } = useLogout();
   const { user } = useAuthContext();
-
-  useEffect(() => {
-    console.log(user);
-    console.log(getLocalStorage("user"));
-  }, [user]);
 
   return (
     <header>
@@ -21,9 +14,17 @@ const NavBar = () => {
         </NavLink>
 
         <nav>
-          <button onClick={() => logout()}>Logout</button>
-          <NavLink to="/login">Login</NavLink>
-          <NavLink to="/signup">Signup</NavLink>
+          {user ? (
+            <>
+              <span>{user.email}</span>
+              <button onClick={() => logout()}>Logout</button>
+            </>
+          ) : (
+            <>
+              <NavLink to="/login">Login</NavLink>
+              <NavLink to="/signup">Signup</NavLink>
+            </>
+          )}
         </nav>
       </div>
     </header>
